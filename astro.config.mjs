dd437e27d6a54cc095be9a865d7f6952340e5840
Pa139300w0rd404 <Abdullah.Sameh40404@gmail.com>
2026-04-22 11:08:12 +0200

    fix astro
    fixing astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://Passw0rd404.github.io",
+  site: 'https://abdullahsameh.qzz.io',
   base: "/Abdullah-Sameh",
   vite: {
     plugins: [tailwindcss()],
