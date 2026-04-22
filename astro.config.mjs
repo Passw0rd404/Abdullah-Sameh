51765cc6bead566476638fa52bd83451fd1b7310
Pa139300w0rd404 <Abdullah.Sameh40404@gmail.com>
2026-04-22 11:32:41 +0200

    fix hot
    hot fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: 'https://abdullahsameh.qzz.io',
-  base: "/Abdullah-Sameh",
   vite: {
     plugins: [tailwindcss()],
   },
